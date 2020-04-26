17a0b63a8ef1f72ac7941fc4cd6d33b5e55b9a34
Sean Carnahan <Sean.Carnahan5@gmail.com>
2020-04-25 23:17:27 -0700

    PREG-16: started work and am now able to pass data between View controllers, going to merge this ticket and then branch off into PREG-14 4/25/20

--- BarBounced/ViewController.swift
+++ BarBounced/ViewController.swift
@@ -9,19 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var addPlayerTextField: UITextField!
+    
+    var player1Name = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //Head Hancho -> first person -> the rest are just ur average honchos
-        
     }
-
-    @IBOutlet weak var addPlayerTextField: UITextField!
-    
     
     @IBAction func addPlayerButton(_ sender: UIButton) {
+        self.player1Name = addPlayerTextField.text!
+        performSegue(withIdentifier: "addedPlayer1Segue", sender: self)
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var vc = segue.destination as! oneAddedPlayerViewController
+        vc.user1 = self.player1Name
+    }
+    
+    
     
 }
 
--- BarBounced/oneAddedPlayerViewController.swift
+++ BarBounced/oneAddedPlayerViewController.swift
@@ -10,20 +10,16 @@ import UIKit
 
 class oneAddedPlayerViewController: UIViewController {
 
+    @IBOutlet weak var playerLabel: UILabel!
+    
+    var user1 = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        playerLabel.text = user1
 
         // Do any additional setup after loading the view.
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
