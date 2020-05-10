5bfb5bc7fb3d0153ad45df31df837c8a7ce50bd2
Sean Carnahan <Sean.Carnahan5@gmail.com>
2020-05-09 18:52:32 -0700

    PREG-32: Randomize amount of game cards before seguing to Personality Warning Slide

---
+++ BarBounced/PersonalityWarningVC.swift
@@ -0,0 +1,23 @@
+//
+//  PersonalityWarningVC.swift
+//  BarBounced
+//
+//  Created by Sean Carnahan on 5/9/20.
+//  Copyright © 2020 Sean Carnahan. All rights reserved.
+//
+
+import UIKit
+
+class PersonalityWarningVC: UIViewController {
+
+    var finalAddedPlayers: [String:String] = [:]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("LOADED PersonalityWarningVC")
+        print(finalAddedPlayers)
+
+        // Do any additional setup after loading the view.
+    }
+}
