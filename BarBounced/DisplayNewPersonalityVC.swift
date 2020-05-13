87c1134812ae65d14c9bd9038c0cbc752f101a53
Sean Carnahan <Sean.Carnahan5@gmail.com>
2020-05-13 13:07:16 -0700

    PREG-43: Create card that notifies users who has what personality

--- BarBounced/DisplayNewPersonalityVC.swift
+++ BarBounced/DisplayNewPersonalityVC.swift
@@ -12,12 +12,66 @@ class DisplayNewPersonalityVC: UIViewController {
     
     var finalAddedPlayers: [playerObject] = []
     var peopleToDisplay : [Int] = []
+    var replacementTitle = ""
+    var isSecondCard = false
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var ruleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        var player1 = peopleToDisplay[0]
+        replacementTitle = finalAddedPlayers[player1].playerName! + " is now " + finalAddedPlayers[player1].personalityTitle!
+        
+        titleLabel.text = replacementTitle
+        ruleLabel.text = finalAddedPlayers[player1].personalityRules
+        
+            //LEFT TAP and RIGHT TAP
+            let touchArea = CGSize(width: self.view.frame.width/2, height: self.view.frame.height)
+            
+            let leftView = UIView(frame: CGRect(origin: .zero, size: touchArea))
+            let rightView = UIView(frame: CGRect(origin: CGPoint(x: self.view.frame.width - touchArea.width, y: 0), size: touchArea))
+        leftView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(leftViewTapped)))
+        rightView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(rightViewTapped)))
 
-        // Do any additional setup after loading the view.
+            self.view.addSubview(leftView)
+            self.view.addSubview(rightView)
     }
-
-
+    
+    @objc func leftViewTapped() {
+        
+        if isSecondCard {
+            //then segue
+        }
+        
+        if peopleToDisplay.count > 1 {
+            displaySecondPerson()
+            
+        } else {
+            //then segue
+        }
+    }
+    
+    @objc func rightViewTapped() {
+        
+        if isSecondCard {
+            //then segue
+        }
+        
+        if peopleToDisplay.count > 1 {
+            displaySecondPerson()
+        } else {
+            //then segue
+        }
+    }
+    
+    func displaySecondPerson(){
+        isSecondCard = true
+        let player2 = peopleToDisplay[1]
+        replacementTitle = finalAddedPlayers[player2 ].playerName! + " is now " + finalAddedPlayers[player2].personalityTitle!
+        
+        titleLabel.text = replacementTitle
+        ruleLabel.text = finalAddedPlayers[player2].personalityRules
+    }
 }
